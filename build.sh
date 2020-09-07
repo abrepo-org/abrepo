236cb6a6207e7a75110ded1daa0763da51698a6c
vergeman <alan.verga@gmail.com>
2020-09-07 16:02:49 -0500

    initial dockr-compose and deploy config

--- build.sh
+++ build.sh
@@ -1,6 +1,22 @@
 #!/usr/bin/env bash
 
+#builds release stack.yml
+DEFAULT_DIR=~/dev/ab/abrepo_ops/releases/abrepo
+mkdir -p $DEFAULT_DIR
+
 #build from app's local Dockerfile
 sudo docker-compose build
 
 sudo `< .env` docker-compose push
+
+#build step, current artifact is just a stack.yml, but in future could be
+#a tarball, etc.
+echo "";
+echo "building artifacts to $DEFAULT_DIR/"
+echo "";
+
+sudo docker-compose -f docker-compose.yml config > $DEFAULT_DIR/stack.yml
+
+echo "";
+ls -g $DEFAULT_DIR
+echo "";
