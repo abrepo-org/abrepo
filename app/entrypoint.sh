1a50b43fb8d2b173c4c6f2128de0a342c9b6c866
vergeman <alan.verga@gmail.com>
2020-12-11 12:10:23 -0600

    upgrade to rails 6, yarn

--- app/entrypoint.sh
+++ app/entrypoint.sh
@@ -4,5 +4,14 @@ set -e
 # Remove a potentially pre-existing server.pid for Rails.
 rm -f /app/tmp/pids/server.pid
 
+# init / update app - move to Dockerfile (build step)
+#cd /app
+#rake db:create
+#rake db:migrate
+#rake assets:precompile
+
+# other startup rake tasks
+#rake xyz
+
 # Then exec the container's main process (what's set as CMD in the Dockerfile).
 exec "$@"
