6c778fe60d3a9cf088444c692efba68624db09d1
vergeman <alan.verga@gmail.com>
2020-12-15 15:27:16 -0600

    remove excess rails engines (action_mailbox, etc.)

--- app/config/application.rb
+++ app/config/application.rb
@@ -1,6 +1,33 @@
 require_relative 'boot'
 
-require 'rails/all'
+#require 'rails/all'
+require "rails"
+
+# This list is here as documentation only - it's not used
+# NB: to enable active_storage: check env files (development.rb)
+# and uncomment config.active_storage lines
+omitted = %w(
+  action_cable/engine
+  action_mailbox/engine
+  action_text/engine
+  active_storage/engine
+)
+
+# Only the frameworks in Rails that do not pollute our routes
+%w(
+  active_record/railtie
+  action_controller/railtie
+  action_view/railtie
+  action_mailer/railtie
+  active_job/railtie
+  rails/test_unit/railtie
+  sprockets/railtie
+).each do |railtie|
+  begin
+    require railtie
+  rescue LoadError
+  end
+end
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
