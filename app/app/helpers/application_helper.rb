d8012fb231098ef0b1a28e88b10ceb21ea426e14
vergeman <alan.verga@gmail.com>
2021-01-07 21:19:02 -0600

    add expvar_date partial; style adjustments

--- app/app/helpers/application_helper.rb
+++ app/app/helpers/application_helper.rb
@@ -1,2 +1,15 @@
 module ApplicationHelper
+
+  def format_expvar_date(utcDate)
+
+    #strftime eg: Jan 8, '21
+    output = utcDate.strftime("%b %e, '%y")
+
+    if Time.current - 3.days < utcDate
+      output = distance_of_time_in_words_to_now(utcDate) + " ago"
+    end
+
+    return output
+  end
+
 end
