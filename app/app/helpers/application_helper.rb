2db0c7198ac2d78244ec0e59371a0db1ccb92c2b
vergeman <alan.verga@gmail.com>
2021-01-14 21:31:52 -0600

    add profile#index table stub view

--- app/app/helpers/application_helper.rb
+++ app/app/helpers/application_helper.rb
@@ -12,4 +12,12 @@ module ApplicationHelper
     return output
   end
 
+  def format_profile_date(utcDate)
+    output = utcDate.strftime("%b %e, %y")
+
+    if Time.current - 3.days < utcDate
+      output = '<i class="far fa-lightbulb"></i>'.html_safe
+    end
+    return output
+  end
 end
