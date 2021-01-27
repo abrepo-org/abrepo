6ac91260b1897796ebe225a0cec1d92f2d5fbe3c
vergeman <alan.verga@gmail.com>
2021-01-27 16:20:46 -0600

    remove commented example code

--- app/app/javascript/js/variations/RenderableContainer.jsx
+++ app/app/javascript/js/variations/RenderableContainer.jsx
@@ -55,12 +55,6 @@ export default class RenderableContainer extends React.Component {
         return false;
     }
 
-    /* function IntersectRect(r1:Rectangle, r2:Rectangle):Boolean {
-     *     return !(r2.left > r1.right
-     *           || r2.right < r1.left
-     *           || r2.top > r1.bottom
-     *           || r2.bottom < r1.top);
-     * }*/
     drawSVGRects() {
 
         const svgStyle = {
