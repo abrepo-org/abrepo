d30a352ce286bed621360c86c0efe949d41c121d
vergeman <alan.verga@gmail.com>
2021-01-27 14:17:23 -0600

    add svg rect effective zIndex via sort order

--- app/app/javascript/js/variations/RenderableContainer.jsx
+++ app/app/javascript/js/variations/RenderableContainer.jsx
@@ -24,6 +24,43 @@ export default class RenderableContainer extends React.Component {
     }
 
 
+    /* returns false if no intersection
+     * otherwise for rectA: maxArea, equalArea, minArea -> 1,0,-1
+     */
+    intersect(rectA, rectB) {
+
+        const val = !(rectB.left > rectA.right ||
+                      rectB.right < rectA.left ||
+                      rectB.top > rectA.bottom ||
+                      rectB.bottom < rectA.top);
+
+        if (val === false) return false;
+        if (val == true) {
+            //maxArea
+            const areaA = rectA.width * rectA.height
+            const areaB = rectB.width * rectB.height
+            if (areaA > areaB) return -1;
+            if (areaA < areaB) return 1;
+        }
+        return 0;
+    }
+
+    contains(rectA, rectB) {
+
+        if (rectA.x < rectB.x && rectA.y < rectB.y) {
+            if ( rectA.x + rectA.width < rectB.x + rectB.width &&
+                 rectA.y + rectA.height < rectB.y + rectB.height)
+                return -1;
+        }
+        return false;
+    }
+
+    /* function IntersectRect(r1:Rectangle, r2:Rectangle):Boolean {
+     *     return !(r2.left > r1.right
+     *           || r2.right < r1.left
+     *           || r2.top > r1.bottom
+     *           || r2.bottom < r1.top);
+     * }*/
     drawSVGRects() {
 
         const svgStyle = {
@@ -35,14 +72,39 @@ export default class RenderableContainer extends React.Component {
             maxHeight: this.state.imgHeight
         };
 
-        const rects = this.props.diffs.map( diff => {
+        const rects = this.props.diffs.sort( (diffA, diffB) => {
+            const dimA = this.state.isControl ? diffA.origDim : diffA.newDim;
+            const dimB = this.state.isControl ? diffB.origDim : diffB.newDim;
+
+            //calc sort order, want larger, containg rects to be rendered first
+            //so subsequent smaller, contained rects have a higher paint order
+            //and are clickable. Effectively sorting for z-index.
+
+            if (!dimA.boundingBox && !dimB.boundingBox) return 0;
+            if (dimA.boundingBox && !dimB.boundingBox) return -1;
+            if (!dimA.boundingBox && dimB.boundingBox) return 1;
+
+            //contains - return the contained
+            const contains = this.contains(dimA.boundingBox.rect, dimB.boundingBox.rect);
+            if (contains === -1) return -1;
+
+            //intersection - greater area first, smaller area later
+            //since ascending: if areaA > areaB, return -1 -> areaA will come before areaB
+            const intersect = this.intersect(dimA.boundingBox.rect, dimB.boundingBox.rect);
+            if (intersect !== false) return intersect;
+
+            //base return y ascending (small -> big)
+            return (dimA.boundingBox.rect.y == dimB.boundingBox.rect.y) ? 0 :
+                   dimA.boundingBox.rect.y > dimB.boundingBox.rect.y ? 1 : -1;
+
+        }).map( diff => {
             return <BoundingBox key={diff.id}
                                 diff={diff}
                                 isControl={this.state.isControl}
                                 diffBboxHoverId={this.props.diffBboxHoverId}
                                 {...this.props}
                    />;
-        });
+        })
 
         return(
             <svg style={svgStyle} className="svg"
