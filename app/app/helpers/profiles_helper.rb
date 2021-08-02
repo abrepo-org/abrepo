a985de6c806e8d709bf2dbed1a262b41e4df64f1
vergeman <alan.verga@gmail.com>
2021-08-02 17:57:07 -0500

    add helper and profiles#show view for related companies like tags

--- app/app/helpers/profiles_helper.rb
+++ app/app/helpers/profiles_helper.rb
@@ -1,2 +1,16 @@
 module ProfilesHelper
+
+  def get_related_companies(profile, num)
+
+    results = profile.active_related_companies(num).collect do |company|
+      {type: :active, company: company}
+    end
+
+    results += profile.inactive_related_companies( num - results.length )
+                 .collect do |company|
+      {type: :inactive, company: company}
+    end
+
+    return results
+  end
 end
