851223a957be69122812b98a9f7c0f63d6f5ca5a
vergeman <alan.verga@gmail.com>
2021-08-13 14:22:08 -0500

    add initial search_controller#show

--- app/app/controllers/search_controller.rb
+++ app/app/controllers/search_controller.rb
@@ -1,7 +1,32 @@
 class SearchController < ApplicationController
 
   def show
-    render json: params
+    #TODO: highlight match snippet
+
+    #
+    # BUILD QUERY
+    #
+
+    #freetext
+    variations = Variation
+
+    if(params[:q])
+      q = params[:q]
+      exp_ids = PgSearch.multisearch(q).pluck(:experiment_id).uniq
+      variations = Variation.where(experiment_id: exp_ids)
+    end
+
+    #filters
+    if (params[:tags])
+      tags = params[:tags]
+      variations = variations.tagged_with(tags)
+    end
+
+    blob = variations.pluck(:id, :summary_name)
+
+    #TODO: what am I returning - experiments and nested variations?
+
+    render json: blob
   end
 
 end
