0d0b1fbacb401160e6714bd40ac8acdad0bde1e9
vergeman <alan.verga@gmail.com>
2021-08-13 16:39:04 -0500

    add industries controller and industries#index route for industry_tags searcH

---
+++ app/app/controllers/industries_controller.rb
@@ -0,0 +1,17 @@
+class IndustriesController < ApplicationController
+
+  def index
+
+    if (params[:tags])
+      tags = params[:tags]
+      @tags = ActsAsTaggableOn::Tag.named_like(tags).for_context('industry_tag')
+      return render json: @tags
+    end
+
+    num = ActsAsTaggableOn::Tag.count
+    @tags = ActsAsTaggableOn::Tag.most_used(num).limit(1000)
+
+    render json: @tags
+  end
+
+end
--- app/config/routes.rb
+++ app/config/routes.rb
@@ -24,6 +24,7 @@ Rails.application.routes.draw do
   resources :profiles, only: ['show', 'index']
   resources :variations, only: ['show']
   resources :tags, only: ['index']
+  resources :industries, only: ['index']
 
   # Search
   get 'search', action: :show, controller: 'search'
