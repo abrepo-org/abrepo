6672636a0d20c390a8f3e60d80a54f82bfa30530
vergeman <alan.verga@gmail.com>
2021-08-18 10:35:27 -0500

    consistent search :query params, demo search#show view remove unused attach div

--- app/app/controllers/search_controller.rb
+++ app/app/controllers/search_controller.rb
@@ -3,7 +3,7 @@ class SearchController < ApplicationController
   def show
     #TODO: highlight match snippet
 
-    query = params[:q]
+    query = params[:query]
     filters = params[:tags]
     industries = params[:industries]
     @results = Search.build(query, filters, industries)
