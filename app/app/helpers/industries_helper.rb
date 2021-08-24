32496de3ce3d1564f358823dc8048138c127b779
vergeman <alan.verga@gmail.com>
2021-08-24 16:35:57 -0500

    update tags, industries tables with various counts

--- app/app/helpers/industries_helper.rb
+++ app/app/helpers/industries_helper.rb
@@ -1,2 +1,10 @@
 module IndustriesHelper
+
+  def calc_tagged_experiments(tag)
+    Profile
+      .includes(:experiments)
+      .tagged_with(tag.name)
+      .inject(0) { |sum, profile| sum + profile.experiments.length }
+  end
+
 end
--- app/app/helpers/tags_helper.rb
+++ app/app/helpers/tags_helper.rb
@@ -1,2 +1,19 @@
 module TagsHelper
+
+  def calc_recency_for(tag, days_ago)
+    #e.g: 10 new experiments in past week
+
+    ActsAsTaggableOn::Tagging
+      .where(tag_id: tag.id)
+      .where('created_at > ?', days_ago)
+      .count
+
+    # TODO refactor out n+1
+    # group_by query, turn to tag_id, count dict
+    # @updates = ActsAsTaggableOn::Tagging
+    #              .where('created_at > (?)', 14.days.ago)
+    #              .group(:tag_id)
+    #              .select("COUNT(tag_id), tag_id").count
+
+  end
 end
