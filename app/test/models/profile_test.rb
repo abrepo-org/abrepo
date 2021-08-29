152fe31deafce444b0d8767427a4bbff7b36a266
vergeman <alan.verga@gmail.com>
2021-08-28 23:45:28 -0500

    add Profile attributes and migration: description, favicon_url, logo_url

--- app/test/models/profile_test.rb
+++ app/test/models/profile_test.rb
@@ -4,7 +4,10 @@
 #
 #  id           :bigint           not null, primary key
 #  company_name :string
+#  description  :string
 #  domain       :string
+#  favicon_url  :string
+#  logo_url     :string
 #  url          :string
 #  created_at   :datetime         not null
 #  updated_at   :datetime         not null
