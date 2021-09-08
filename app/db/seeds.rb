7dfcabb62807fb0cbebeec9647ce272f1775bd5e
vergeman <alan.verga@gmail.com>
2021-09-08 18:13:00 -0500

    add importer user as moderator to db.seeds

--- app/db/seeds.rb
+++ app/db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+#
+# add initial "importer" user with moderator priviledges
+#
+
+user = User.where(email: "importer@abrepo.com")
+
+if (user.empty?)
+  user = User.where(email: "importer@abrepo.com",
+                    password: ENV['USER_IMPORTER_PASSWORD'],
+                    moderator: true).new
+  user.save
+else
+  user.update(moderator:true)
+end
