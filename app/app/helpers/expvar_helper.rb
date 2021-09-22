5aa397c08694b8dfb83449b22e65b9d441b6faac
vergeman <alan.verga@gmail.com>
2021-09-22 14:23:38 -0500

    add temp variation attributes for expvar_table; expvar_helper set_multiple_views to label variations revised display logic in expvar_table

--- app/app/helpers/expvar_helper.rb
+++ app/app/helpers/expvar_helper.rb
@@ -1,12 +1,39 @@
 module ExpvarHelper
 
-  # determines if variation-view is the same variation (should be hidden) with a
-  # different view, or is a standalone variation
-  # TODO: assumes variations sorted by vendor_id - set sort in imports?
+  # determines if variation-view is the same variation (should be
+  # hidden) with a different view, or is a standalone variation
+  #
+  # is_root and multiple_views -> caret
+  # is_root and not multiple_views-> vanilla variation (no caret, no hide)
+  # !is_root and multiple_views ->  hidden child
+  def set_multiple_views(variations, index)
 
-  def isVarDupeView(variations, index)
-    return false if index == 0
-    return variations[index].vendor_id == variations[index-1].vendor_id
+    num_vendor_id = num_by_vendor_id(variations, index)
+
+    if index == 0
+      variations[index].multiple_views = true ? num_vendor_id > 1 : false
+      variations[index].is_root = true
+      return
+    end
+
+    if (variations[index].vendor_id != variations[index-1].vendor_id)
+
+      # first of its (multiple) kind? its a root
+      if ( num_vendor_id > 1)
+        variations[index].multiple_views = true
+        variations[index].is_root = true
+      else
+        # first of its kind but no children, vanilla standalone variation
+        variations[index].multiple_views = false
+        variations[index].is_root = true
+      end
+
+    end
+  end
+
+  def num_by_vendor_id(variations, index)
+    vendor_id = variations[index].vendor_id
+    variations.where(vendor_id: vendor_id).count
   end
 
   def pageURLHelper(variation)
