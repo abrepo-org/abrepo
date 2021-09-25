219e5f3c95958d5328aa89e4500bd2772020e2cd
vergeman <alan.verga@gmail.com>
2021-09-25 14:57:12 -0500

    add user_saved_variations_hash helper to test records from a single query

--- app/app/controllers/user_saved_variations_controller.rb
+++ app/app/controllers/user_saved_variations_controller.rb
@@ -2,16 +2,16 @@ class UserSavedVariationsController < ApplicationController
   before_action :authenticate_user!
 
   def create
-    status = false
-    puts current_user.id, params[:id]
-    @uv = current_user.user_saved_variations.build({variation_id: params[:id]})
-    puts @uv.inspect
 
-    # if exists set delete false / else true
-    # has_many, dependent: :destroy
-    # status = true if @uv.save
-    # status = true
+    @uv = current_user.user_saved_variations.find_by_variation_id(params[:id])
 
+    if (@uv)
+      @uv.update_attribute(:deleted, !@uv.deleted)
+    else
+      @uv = current_user.user_saved_variations.create(variation_id: params[:id])
+    end
+
+    status = !@uv.deleted
 
     respond_to do |format|
       format.json  { render json: {saved: status}  }
