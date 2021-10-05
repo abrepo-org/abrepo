ecc7d934d80dfcddfcf9ea539fd349232942014f
vergeman <alan.verga@gmail.com>
2021-10-04 23:59:24 -0500

    add uniqueness validation to profile (domain and a_id)

--- app/app/models/profile.rb
+++ app/app/models/profile.rb
@@ -53,7 +53,7 @@ class Profile < ApplicationRecord
                           foreign_key: "profile_id",
                           association_foreign_key: "related_profile_id"
 
-  validates :domain, :a_id, presence: true
+  validates :domain, :a_id, presence: true, uniqueness: true
 
 
   def active_related_companies(num)
