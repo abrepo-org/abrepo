2406416ca435cfff03d974251131699915a70b8a
vergeman <alan.verga@gmail.com>
2021-10-15 17:28:33 -0500

    add authorization obfuscation check, and obfuscate_all() collection controller & view helper

--- app/app/controllers/application_controller.rb
+++ app/app/controllers/application_controller.rb
@@ -1,4 +1,22 @@
 class ApplicationController < ActionController::Base
   include Pundit
   include Pagy::Backend
+
+  def subscribed_or_moderator
+    (user_signed_in? &&
+     (current_user.subscribed? || current_user.moderator?))
+  end
+
+
+  def obfuscate_all(instances, num_start)
+    instances.each_with_index do |instance, index|
+      instance.obfuscate if index >= num_start && instance.respond_to?('obfuscate')
+    end
+
+    instances
+  end
+
+
+  helper_method :subscribed_or_moderator, :obfuscate_all
+
 end
