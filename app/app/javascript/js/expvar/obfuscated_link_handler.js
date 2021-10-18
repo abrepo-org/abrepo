892ae29c4d918c3c4ce2d833aa27eb597f075a01
vergeman <alan.verga@gmail.com>
2021-10-18 17:22:22 -0500

    add ESC to close on subscription modal

--- app/app/javascript/js/expvar/obfuscated_link_handler.js
+++ app/app/javascript/js/expvar/obfuscated_link_handler.js
@@ -5,10 +5,23 @@ function obfuscated_click_handler($links) {
         $link.addEventListener('click', (e) => {
             e.preventDefault();
 
-            document
-                .querySelector('#subscribe-modal')
-                .classList
+            const $modal = document
+                  .querySelector('#subscribe-modal');
+
+            $modal.classList
                 .toggle('is-active');
+
+
+            //add ESC
+            const keyPress = (e) => {
+
+                if(e.key === "Escape") {
+                    $modal.classList.toggle('is-active');
+                    document.removeEventListener('keydown', keyPress);
+                }
+            };
+
+            document.addEventListener('keydown', keyPress);
         });
 
     });
--- app/app/javascript/js/modal/subscribe_modal.js
+++ app/app/javascript/js/modal/subscribe_modal.js
@@ -2,24 +2,28 @@ document.addEventListener('DOMContentLoaded', () => {
 
     const $modal = document.querySelector('#subscribe-modal');
 
-    /*
-    const keyPress = (e) => {
-        console.log("KP", e);
-        if(e.key === "Escape") {
-            $modal.classList.toggle('is-active');
-            $modal.removeEventListener(keyPress);
-        }
-    };
-
-    $modal.addEventListener('keypress', keyPress);
-    */
-
     //close button click
     $modal
         .querySelector('.modal-close')
         .addEventListener('click', (e) => {
 
-        $modal.classList.toggle('is-active');
+            $modal.classList.toggle('is-active');
     });
+
+
+    //initially open on load (cookie)
+    if($modal.classList.contains('is-active')) {
+
+        //add ESC
+        const keyPress = (e) => {
+
+            if(e.key === "Escape") {
+                $modal.classList.toggle('is-active');
+                document.removeEventListener('keydown', keyPress);
+            }
+        };
+
+        document.addEventListener('keydown', keyPress);
+    }
 
 });
