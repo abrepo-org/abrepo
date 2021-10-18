2dd3c53615df12cd0f68ff92b20693815e89d285
vergeman <alan.verga@gmail.com>
2021-10-18 12:34:49 -0500

    add expvar helper obfuscated_link_to to generate vanilla or obfuscated link

--- app/app/helpers/expvar_helper.rb
+++ app/app/helpers/expvar_helper.rb
@@ -1,5 +1,12 @@
 module ExpvarHelper
 
+  def obfuscate_link_to(obfuscated, path, &block)
+    path = "#" if obfuscated
+    link_to(path, class: obfuscated ? "obfuscated-link" : "" ) do
+      yield
+    end
+  end
+
   # determines if variation-view is the same variation (should be
   # hidden) with a different view, or is a standalone variation
   #
