63a77ecc2da434a1eff92a5b82dca9a3c8173e37
vergeman <alan.verga@gmail.com>
2021-10-28 10:55:26 -0500

    add role3 typewriter animation

--- app/app/javascript/js/landing/animation.js
+++ app/app/javascript/js/landing/animation.js
@@ -6,9 +6,9 @@
  *
  * In cases where there is no delay, don't need to trigger show/hide
  * but rely on animation fade.
- */
 
-document.addEventListener('DOMContentLoaded', () => {
+
+ document.addEventListener('DOMContentLoaded', () => {
 
     const $role1a = document.querySelectorAll('.role-1-a');
     const $role1b = document.querySelectorAll('.role-1-b');
@@ -18,4 +18,30 @@ document.addEventListener('DOMContentLoaded', () => {
             //$role.classList.toggle('is-invisible');
         }, {once:true});
     });
+});
+*/
+
+
+
+/* typewriter animation role3 */
+document.addEventListener('DOMContentLoaded', () => {
+
+    const $input = document.querySelector('.role-3 input');
+    if (!$input) return;
+
+    //init txt value to be "typed"
+    const txt = $input.dataset.value;
+
+    let i = 0;
+    const typeWrite = () => {
+
+        //reset
+        if (i == 0) $input.value = '';
+
+        $input.value += txt.charAt(i);
+        i = (++i % txt.length);
+    };
+
+    window.setInterval(typeWrite, 120);
+
 });
