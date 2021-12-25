76dd762df1a43c4cd501c6654ccff6e599f707e9
vergeman <alan.verga@gmail.com>
2021-12-25 16:32:51 -0600

    reconfigure Devise parent mailer to use ApplicationMailer; catch mailer errors and log

--- app/app/mailers/application_mailer.rb
+++ app/app/mailers/application_mailer.rb
@@ -1,4 +1,26 @@
 class ApplicationMailer < ActionMailer::Base
-  default from: 'from@example.com'
-  layout 'mailer'
+
+  # want to log delivery errors
+  # but proceed as if they weren't raised
+  # (config.action_mailer.raise_delivery_errors = false)
+  rescue_from EOFError,
+              IOError,
+              TimeoutError,
+              Errno::ECONNRESET,
+              Errno::ECONNABORTED,
+              Errno::EPIPE,
+              Errno::ETIMEDOUT,
+              Net::SMTPAuthenticationError,
+              Net::SMTPServerBusy,
+              Net::SMTPSyntaxError,
+              Net::SMTPUnknownError,
+              Net::SMTPFatalError,
+              OpenSSL::SSL::SSLError,
+              Exception do |e|
+
+    Rails.logger.error "Mailer Exception"
+    Rails.logger.error e.inspect
+  end
+
+
 end
