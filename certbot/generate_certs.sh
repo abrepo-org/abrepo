cb3d1e6e865e831b8fca15d5233ea78847a46785
Alan Verga <alan.verga@gmail.com>
2022-01-06 14:04:23 -0600

    Topic/ssl certs (#82)
    * initial certbot build and copy, generation notes
    
    * nginx ssl config; port open 443 and change 80 to internal expose
    
    * build.sh create haproxy combined cert
    
    * change error ssl bitnami haproxy to working community image
    adjust mounts for haproxy dev/prod config
    
    * update haproxy prod/staging ports (443 -> 8888) host mode configuration
    
    * move nginx to serve on 8080 in prod; add server config README notes and minor certbot notes
    
    * update haproxy resolver config; add frontend admin
    
    * add generate_certs.sh script

---
+++ certbot/generate_certs.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+#
+# for wildcard *.abrepo.com
+#
+sudo docker run \
+     -v /home/vergeman/dev/ab/abrepo/certbot/letsencrypt:/etc/letsencrypt \
+     -it certbot/dns-cloudflare:latest certonly --dns-cloudflare \
+     --dns-cloudflare-credentials /etc/letsencrypt/cloudflare.ini \
+     -d *.abrepo.com
+
+
