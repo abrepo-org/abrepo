3de6bd0e0121c611ccdb71c3d7a574b7c0173137
vergeman <alan.verga@gmail.com>
2022-07-21 10:37:58 -0500

    add initial format_css and render functions in DiffSummary component

--- app/app/javascript/js/variations/diff/DiffSummary.jsx
+++ app/app/javascript/js/variations/diff/DiffSummary.jsx
@@ -4,13 +4,28 @@ const DiffSummary = (props) => {
 
     if (!props.summary) return null;
 
-    const cssRegExp = 'rgb\(\s*\d+,\s*\d+,\s*\d+\);?';
+    //TODO: add notes highlighting detection (abanno) and render functions (here)
+    const format_css = (diff, summary_delta, summary, summary_format) => {
 
-    const format_css = (summary) => {
+        if (!summary_format) return summary;
+
+        if (!summary_format.toLocaleLowerCase().includes('css')) return summary;
+
+        summary = summary.split(";").join(';<br/>');
+
+        return summary;
+    };
+
+    /*
+     * replaces css text 'rgb(...' with icon and color
+     */
+    const format_rgb_css = (summary) => {
 
         if (!summary.includes('rgb(')) return summary;
 
-        const s = summary.split(/(rgb\(\s*\d+,\s*\d+,\s*\d+\));?/);
+        const cssRegExp = /(rgb\(\s*\d+,\s*\d+,\s*\d+\));?/;
+
+        const s = summary.split(cssRegExp);
 
         let flag = false;
         for (let i = 0; i < s.length; i++) {
@@ -21,7 +36,8 @@ const DiffSummary = (props) => {
                 s[i-1] = <span>{s[i-1]}</span>;
                 s[i] =
                     <>
-                    <span class='cssbox' title={rgb} style={style}>&nbsp;&nbsp;</span>
+                    <span className='cssbox'
+                          title={rgb} style={style}>&nbsp;&nbsp;</span>
                     <span>{rgb}</span>
                     <br/>
                     </>;
@@ -125,11 +141,29 @@ const DiffSummary = (props) => {
 
     let summary = format_truncate( props.summary, !props.detail );
     try {
-        summary = format_css(summary);
+        summary = format_css(props.diff, props.diff.summary_delta, summary, props.summary_format);
+        summary = format_rgb_css(summary);
     } catch(e) {
         console.error("format_css err");
     }
 
+    const renderSummary =(summary, summary_format) => {
+
+        if (summary_format == "css") {
+            return(
+                <div className='diff-summary summary-text pl-2'
+                     dangerouslySetInnerHTML={{__html: summary}}>
+                </div>
+            );
+        }
+
+        return(
+            <div className='diff-summary summary-text pl-2'>
+              {summary}
+            </div>
+        );
+
+    };
 
     const paddingTop = props.nonVisible ? "pt-1" : "pt-2";
 
@@ -138,9 +172,7 @@ const DiffSummary = (props) => {
           <div className={`${props.colorClass}`}>
             {props.icon}
           </div>
-          <div className='diff-summary summary-text pl-2'>
-            {summary}
-          </div>
+          {renderSummary(summary, props.summary_format)}
         </div>
     );
 };
