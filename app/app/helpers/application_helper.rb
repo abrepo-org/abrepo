cd981340ffebd2becfde5af0e1fe8a37aab25bb7
Alan Verga <alan.verga@gmail.com>
2022-07-21 13:35:51 -0500

    Feat/expire fragment caches (#98)
    * application_helper.rb: revise format_expvar_date() to use stftime only, avoid caching mess
    
    * views fragment cache: set 1.day cache expiry

--- app/app/helpers/application_helper.rb
+++ app/app/helpers/application_helper.rb
@@ -29,11 +29,10 @@ module ApplicationHelper
   def format_expvar_date(utcDate)
 
     #strftime eg: Jan 8, '21
+    #
+    #NB: avoid distance_of_time_in_words_to_now type methods
+    #to favor fragment caching
     output = utcDate.strftime("%b %e, '%y")
-
-    if Time.current - 3.days < utcDate
-      output = distance_of_time_in_words_to_now(utcDate) + " ago"
-    end
 
     return output
   end
