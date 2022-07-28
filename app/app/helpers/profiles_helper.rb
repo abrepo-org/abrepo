3259756e2cfd3d38903eee94eb74f1e3f3ae4f03
Alan Verga <alan.verga@gmail.com>
2022-07-28 17:03:15 -0500

    Topic/profile related industry fixes (#105)
    * profile.rb: refactor out get_related_companies() view helper to model method
    
    * profile#show: implement proper checks for empty variables; about, companies, industry

--- app/app/helpers/profiles_helper.rb
+++ app/app/helpers/profiles_helper.rb
@@ -22,17 +22,4 @@ module ProfilesHelper
     }.join()
   end
 
-  def get_related_companies(profile, num)
-
-    results = profile.active_related_companies(num).collect do |company|
-      {type: :active, company: company}
-    end
-
-    results += profile.inactive_related_companies( num - results.length )
-                 .collect do |company|
-      {type: :inactive, company: company}
-    end
-
-    return results
-  end
 end
