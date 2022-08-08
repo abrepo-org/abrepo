d35c450f5e2b25e57294131695f2c4c87ee02e65
vergeman <alan.verga@gmail.com>
2022-08-08 14:58:42 -0500

    remove Variation tag_list, page_tag_list fields from multisearch index

--- app/app/models/variation.rb
+++ app/app/models/variation.rb
@@ -27,8 +27,7 @@ class Variation < ApplicationRecord
   include Obfuscatable
   include PgSearch::Model
 
-  multisearchable against: [:summary_name, :tag_list, :page_tag_list],
-                  #against: [:summary_name],
+  multisearchable against: [:summary_name],
                   additional_attributes: -> (variation) {{ experiment_id: variation.experiment_id }}
 
   pg_search_scope :search_tag,
