34a5d15481f0787de8ee24d2c1ed32c6ed3302fb
vergeman <alan.verga@gmail.com>
2022-08-12 19:33:00 -0500

    /search fix empty query? param in selected tags removal link

--- app/app/helpers/search_helper.rb
+++ app/app/helpers/search_helper.rb
@@ -3,7 +3,8 @@ module SearchHelper
 
   def tag_link_for(query, tags, industries)
     search_input_text = Search.buildSearchQuery(query, tags, industries)
-    url_for(params: { query: search_input_text })
+    return url_for(params: { query: search_input_text }) unless search_input_text.blank?
+    return nil
   end
 
 end
