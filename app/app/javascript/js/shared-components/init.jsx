a08177a7345a769e3adde03537391c518524726f
vergeman <alan.verga@gmail.com>
2022-08-12 19:52:18 -0500

    set consistent div wrappers, spacing across /profiles, /tags, /industries

--- app/app/javascript/js/shared-components/init.jsx
+++ app/app/javascript/js/shared-components/init.jsx
@@ -15,6 +15,7 @@ document.addEventListener('DOMContentLoaded', () => {
     const $profileForm = document.querySelector('form#profile-form');
 
     const $inputTemp = document.createElement('div');
+    $inputTemp.style.cssText="margin-bottom:2.25rem;";
 
     const render = ($form, props) => {
         ReactDOM.render(
