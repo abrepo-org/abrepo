0d092b70970bd5d17e3c7705bcc9e80510c059fa
vergeman <alan.verga@gmail.com>
2022-08-19 17:11:19 -0500

    landing#index to query hero_companies using hard-coded domains; refactor partials to loop @profiles

--- app/app/controllers/landing_controller.rb
+++ app/app/controllers/landing_controller.rb
@@ -7,6 +7,22 @@ class LandingController < ApplicationController
     # flash[:alert] = "Currently in maintenance mode"
     #
 
+    @profiles = []
+    @tags = []
+
+    # TODO: change to query Profiles with Experiments > 3 + logo?
+    domains = [
+      'showtime.com', 'bigcommerce.com', 'aspca.org', 'hostgator.com', 'elastic.com', 'freshdesk.com', 'sofi.com', 'mongodb.com'
+    ]
+
+    if ENV['RAILS_ENV'] == "development"
+      domains = [
+        'bigcommerce.com', 'zillow.com','gartner.com', 'slack.com', 'mindbody.com', 'change.org', 'doordash.com', 'hootsuite.com']
+    end
+
+    @profiles = Profile.where(domain: domains)
+    @tags =[]
+
     #NB: helper currently returns 'basic-monthly', 'basic-annual' lookup keys
     @basic_monthly_price, @basic_annual_price = get_all_stripe_data
   end
