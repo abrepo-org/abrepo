b14418169c0574383a726283dc4c4fafb1be3068
Alan Verga <alan.verga@gmail.com>
2022-08-20 17:13:01 -0500

    Feat/landing 2.0 (#117)
    * update landing panel3 with tag color; remove links
    
    * revised role2 for split image display
    
    * reorder roles and invert role 3 layout
    
    * switch role1 -> role2 filenames
    
    * reformat role1.html.erb
    
    * role2 refactor; add initial expvar overlay
    
    * adjust role-2 landing
    
    * refactor role3 -> role_filter
    
    * refactor role1 -> role_parallel
    
    * role2 -> role-parallel classes and css
    
    * change references from role2 -> role_before_after
    
    * initial hero revision stub
    
    * refactor landing stubs to partials with local params
    
    * landing#index to query hero_companies using hard-coded domains; refactor partials to loop @profiles
    
    * add hero tags listing
    
    * add form_with helper to enable input search submission
    
    * add inline js click handler to toggle search input dropdown on landing page only
    
    * add search bar dropdown links (hardcoded) with right-align descriptions
    
    * landing page add hr's after section
    
    * add CTAs on landing roles
    
    * refactor out partial role_before_after_expvar_table; minor css resets
    
    * revised role_before_after layout + responsiveness; new dog img
    
    * minor mobile spacing adjusts
    
    * fix landing page profile_path helper to use profile_path_slug

--- app/app/controllers/landing_controller.rb
+++ app/app/controllers/landing_controller.rb
@@ -7,6 +7,37 @@ class LandingController < ApplicationController
     # flash[:alert] = "Currently in maintenance mode"
     #
 
+    @profiles = []
+
+    #[query, description] pairs
+    @search_queries = [
+      ['CTA [hero]', '"CTA" experiments tagged with "hero"'],
+      ['navbar {internet}', '"navbar" experiments within "internet" industry'],
+      ['redirect', '"redirect" experiments'],
+      ['header [signup-page]', '"header" experiments on "signup-page"'],
+      ['webinar', '"webinar" experiments']
+    ]
+
+    # TODO: change to query Profiles with Experiments > 3 + logo?
+    # Tags are hardcoded in /landing/_hero_tags.html.erb (expensive
+    # queries to discern taggable_type - industry/variation to build
+    # query url)
+
+    domains = [ 'showtime.com', 'bigcommerce.com', 'aspca.org',
+    'hostgator.com', 'elastic.com', 'freshdesk.com', 'sofi.com',
+    'mongodb.com' ]
+
+    if ENV['RAILS_ENV'] == "development"
+
+      domains = [ 'bigcommerce.com', 'zillow.com','gartner.com',
+        'slack.com', 'mindbody.com', 'change.org', 'doordash.com',
+        'hootsuite.com']
+
+    end
+
+    @profiles = Profile.where(domain: domains)
+
+
     #NB: helper currently returns 'basic-monthly', 'basic-annual' lookup keys
     @basic_monthly_price, @basic_annual_price = get_all_stripe_data
   end
