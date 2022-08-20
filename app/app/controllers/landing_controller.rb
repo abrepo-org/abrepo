f4d7f1666a31f77bf01894f011a5b4edda8cde90
vergeman <alan.verga@gmail.com>
2022-08-19 22:05:29 -0500

    add search bar dropdown links (hardcoded) with right-align descriptions

--- app/app/controllers/landing_controller.rb
+++ app/app/controllers/landing_controller.rb
@@ -9,18 +9,30 @@ class LandingController < ApplicationController
 
     @profiles = []
 
+    #[query, description] pairs
+    @search_queries = [
+      ['CTA [hero]', '"CTA" experiments tagged with "hero"'],
+      ['navbar {internet}', '"navbar" experiments within "internet" industry'],
+      ['redirect', '"redirect" experiments'],
+      ['header [signup-page]', '"header" experiments on "signup-page"'],
+      ['webinar', '"webinar" experiments']
+    ]
+
     # TODO: change to query Profiles with Experiments > 3 + logo?
     # Tags are hardcoded in /landing/_hero_tags.html.erb (expensive
     # queries to discern taggable_type - industry/variation to build
     # query url)
 
-    domains = [
-      'showtime.com', 'bigcommerce.com', 'aspca.org', 'hostgator.com', 'elastic.com', 'freshdesk.com', 'sofi.com', 'mongodb.com'
-    ]
+    domains = [ 'showtime.com', 'bigcommerce.com', 'aspca.org',
+    'hostgator.com', 'elastic.com', 'freshdesk.com', 'sofi.com',
+    'mongodb.com' ]
 
     if ENV['RAILS_ENV'] == "development"
-      domains = [
-        'bigcommerce.com', 'zillow.com','gartner.com', 'slack.com', 'mindbody.com', 'change.org', 'doordash.com', 'hootsuite.com']
+
+      domains = [ 'bigcommerce.com', 'zillow.com','gartner.com',
+        'slack.com', 'mindbody.com', 'change.org', 'doordash.com',
+        'hootsuite.com']
+
     end
 
     @profiles = Profile.where(domain: domains)
