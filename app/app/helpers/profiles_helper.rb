4f6c0c75662249a383f7fbf62145a3b98051ede3
vergeman <alan.verga@gmail.com>
2022-08-30 18:22:47 -0500

    revise profile helpers and views to use expvar_highlight_for

--- app/app/helpers/profiles_helper.rb
+++ app/app/helpers/profiles_helper.rb
@@ -2,37 +2,26 @@ module ProfilesHelper
 
   MAX_LEN = 200
 
-  def profile_company_name(profile, names_map, domains_map)
-    if names_map && names_map[profile.id]
-      return sanitize names_map[profile.id].pg_search_highlight
-    end
-
-    if profile.company_name
-      return profile.company_name
-    end
-
-    if domains_map && domains_map[profile.id]
-      return sanitize domains_map[profile.id].pg_search_highlight
-    end
-
-    return profile.domain
-  end
-
-
-  def profile_description(profile, descriptions_map)
+  def profile_description(profile, attributeHighlightHash)
 
     # search/filter: return highlighted results
-    if descriptions_map && descriptions_map[profile.id]
-      return sanitize descriptions_map[profile.id]
-               .pg_search_highlight
-
+    # change description to center around search result
+    if !attributeHighlightHash.empty?
+      return expvar_highlight_for(profile, attributeHighlightHash,
+                                  :description,
+                                  profile.description)
     end
 
     # empty
     return nil unless profile.description?
 
-    # find nearest sentence to MAX_LEN chars
     # used on profiles#index
+    #
+    # find nearest sentence to MAX_LEN chars
+    # want to generate a summary snippet ~ MAX_LEN length -
+    # that's shorter than profile#show; and also *not* a search
+    # snippet centered about the search term.
+    #
     # 1. find positions of 'period' in description
     # 2. find position nearest to MAX_LEN (shortest distance)
     # 3. return substring from 0 to that position + 1 (for period)
@@ -42,7 +31,6 @@ module ProfilesHelper
 
     index = res[ res.map { |r| (r - MAX_LEN).abs }.each_with_index.min[1] ]
     return profile.description[0, index + 1]
-
 
   end
 
