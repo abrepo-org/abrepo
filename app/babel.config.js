a0aca2113fa333864efcdf10f50fc03d0b2655a0
vergeman <alan.verga@gmail.com>
2025-01-08 10:49:57 -0600

    fix(web): errant formatting

--- app/babel.config.js
+++ app/babel.config.js
@@ -34,8 +34,8 @@ module.exports = function(api) {
           useBuiltIns: 'entry',
           corejs: 3,
           modules: false,
-            exclude: ['transform-typeof-symbol'],
-            loose: true
+          exclude: ['transform-typeof-symbol'],
+          loose: true
         }
       ],
       [
