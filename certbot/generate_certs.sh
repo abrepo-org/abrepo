eb8901a472e75f01aec42f2b152ce0da9d006599
vergeman <alan.verga@gmail.com>
2025-01-08 18:32:21 -0600

    chore(web) /certbot cleanup
    certbot/README.md formatting
    adjust .gitignore to add cloudlfare.ini.stub

--- certbot/generate_certs.sh
+++ certbot/generate_certs.sh
@@ -3,10 +3,10 @@
 #
 # for wildcard *.abrepo.com
 #
-sudo docker run \
-     -v /home/vergeman/dev/ab/abrepo/certbot/letsencrypt:/etc/letsencrypt \
-     -it certbot/dns-cloudflare:latest certonly --dns-cloudflare \
-     --dns-cloudflare-credentials /etc/letsencrypt/cloudflare.ini \
-     -d *.abrepo.com
+docker run \
+       -v /home/vergeman/dev/ab/abrepo/certbot/letsencrypt:/etc/letsencrypt \
+       -it certbot/dns-cloudflare:latest certonly --dns-cloudflare \
+       --dns-cloudflare-credentials /etc/letsencrypt/cloudflare.ini \
+       -d *.abrepo.com
 
 
