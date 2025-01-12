7cd9b7ba0fcd10ce8d5d79ba8e80c66c12eea56a
vergeman <alan.verga@gmail.com>
2025-01-11 23:10:53 -0600

    feat(web): add simplecov coverage reporting

--- app/test/test_helper.rb
+++ app/test/test_helper.rb
@@ -1,6 +1,12 @@
 ENV['RAILS_ENV'] ||= 'test'
 require_relative '../config/environment'
 require 'rails/test_help'
+require 'simplecov'
+
+SimpleCov.start 'rails' do
+  add_filter 'bundler/'
+  track_files '{app}/**/*.rb' # Only track app files
+end
 
 class ActiveSupport::TestCase
   # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
