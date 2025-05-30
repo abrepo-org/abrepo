9833c5114904b5e5738ffcb38155b954cecfda8e
vergeman <alan.verga@gmail.com>
2025-05-30 11:43:44 -0500

    feat(web): remove stripe session creation post user creation flow skip Stripe Checkout Session call and disable js-Stripe subscription button handler

--- app/app/controllers/checkout_controller.rb
+++ app/app/controllers/checkout_controller.rb
@@ -57,23 +57,27 @@ class CheckoutController < Devise::RegistrationsController
         # want email to be auto sent to stripe (less friction)
         #
 
-        @price_key = params_price_key
-        @price = get_stripe_data(@price_key)
-        session = nil
+        # --- Stripe ---
+        # @price_key = params_price_key
+        # @price = get_stripe_data(@price_key)
+        # session = nil
 
-        begin
-          session = purchase_stripe(@price.id, @price_key)
-        rescue => e
-          message = "Payment provider error. Please try again."
-          render status: 400, json: { user: {ok: true, errors: false},
-                                      stripe: { ok: false, errors: { messages: [ message ] } }}
-          return
-        end
+        # begin
+        #   session = purchase_stripe(@price.id, @price_key)
+        # rescue => e
+        #   message = "Payment provider error. Please try again."
+        #   render status: 400, json: { user: {ok: true, errors: false},
+        #                               stripe: { ok: false, errors: { messages: [ message ] } }}
+        #   return
+        # end
 
-        render status: 200, json: { user: { ok: true, errors: false },
-                                    stripe: { ok: true, errors: false, sessionId: session.id }}
-        return
-
+        # render status: 200, json: { user: { ok: true, errors: false },
+        #                             stripe: { ok: true, errors: false, sessionId: session.id }}
+        # return
+      # --- End Stripe ---
+        render status: 200, json: { location: after_sign_up_path_for(resource),
+                                    user: {ok: true, errors: false},
+                                    stripe: { ok: false, errors: { messages: resource.errors.full_messages } }}
       else
 
         expire_data_after_sign_in!
